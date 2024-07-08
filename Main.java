c13f3a54c699f85e94a39d9b0efb065409dae0cf
yuzuki <y.yamahata.sys23@morijyobi.ac.jp>
2024-07-08 11:34:39 +0900

    PlayerクラスとMainクラスを実装した close #7,#8,#9

---
+++ Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+
+   public static void main(String[] args) {
+
+       Player user = new Player("ユーザ");
+
+       Player com = new Player("CPU");
+
+
+       //入力指示表示
+
+       System.out.println("1.グー");
+
+       System.out.println("2.チョキ");
+
+       System.out.println("3.パー");
+
+       System.out.println("あなたの手を選択してください。>");
+
+   }
+
+}
