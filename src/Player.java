fba5daba1ce5c44873c009868e0589586bc16592
yuzuki <y.yamahata.sys23@morijyobi.ac.jp>
2024-07-08 11:54:05 +0900

    push

---
+++ src/Player.java
@@ -0,0 +1,23 @@
+public class Player {
+
+   private String playerName;
+
+   public Player(String playerName) {
+
+       this.playerName = playerName;
+
+   }
+
+   public String getPlayerName() {
+
+       return playerName;
+
+   }
+
+   public void setPlayerName(String playerName) {
+
+       this.playerName = playerName;
+
+   }
+
+}
