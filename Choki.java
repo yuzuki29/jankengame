aea23304e729546f316bd577bf22cb12ae0ee42c
yuzuki <y.yamahata.sys23@morijyobi.ac.jp>
2024-07-19 10:57:46 +0900

    修正

--- Choki.java
+++ Choki.java
@@ -1,40 +1,36 @@
 public class Choki implements Hand {
 
-   private String HAND_NAME = "チョキ";
+    private String HAND_NAME = "チョキ";
 
 
-
-   /**
-
-    手の強弱を比較する。
-
-    返り値 -1 : 自身が引数で指定した手より弱い
-
-    　　　　0 : 自身が引数で指定した手と同じ
-
-    　　　　1 : 自身が引数で指定した手より強い
-
-    */
+    /**
+     * 手の強弱を比較する。
+     * <p>
+     * 返り値 -1 : 自身が引数で指定した手より弱い
+     * <p>
+     * 　　　　0 : 自身が引数で指定した手と同じ
+     * <p>
+     * 　　　　1 : 自身が引数で指定した手より強い
+     */
 
 
-   public int compare(Hand hand) {
+    public int compare(Hand hand) {
 
-       if(hand instanceof Gu) {    //グー　vs チョキ（自分）
+        if (hand instanceof Gu) {    //グー　vs チョキ（自分）
 
-           return -1;
+            return -1;
 
-       } else if(hand instanceof Pa) {    //パー　vs チョキ（自分）
+        } else if (hand instanceof Pa) {    //パー　vs チョキ（自分）
 
-           return 1;
+            return 1;
 
-       } else {
+        } else {
 
-           return 0;    //グー　vs グー（自分）
+            return 0;    //グー　vs グー（自分）
 
-       }
+        }
 
-   }
-
+    }
 
 
     public String getHandName() {
@@ -42,3 +38,4 @@ public class Choki implements Hand {
         return this.HAND_NAME;
 
     }
+}
